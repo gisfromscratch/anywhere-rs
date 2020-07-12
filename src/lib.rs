b8304e05a816296becdc2d9d3257c47ceb137df5
J.T <gisfromscratch@live.de>
2020-07-12 14:04:10 +0200

    #1 Dummy payload test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
+
+pub struct Portal {
+    client_id: String,
+    client_secret: String
+}
+
+impl Portal {
+
+    pub fn new (client_id: String, client_secret: String) -> Self {
+        Portal { client_id, client_secret }
+    }
+
+    pub fn generate_token(&self) -> String {
+        let mut payload: String = "client_id=".to_owned();
+        payload.push_str(&self.client_id);
+        payload.push_str("&client_secret=");
+        payload.push_str(&self.client_secret);
+        payload.push_str("&grant_type=client_credentials");
+        payload
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn generate_token() {
+        let portal = Portal { 
+            client_id: "123".to_owned(), 
+            client_secret: "xyz".to_owned()
+        };
+
+        let expected_payload: &str = "client_id=123&client_secret=xyz&grant_type=client_credentials";
+        let payload = portal.generate_token();
+        assert_eq!(expected_payload, payload);
     }
 }
